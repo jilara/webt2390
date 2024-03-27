701883015e33d454055cc765aad72fbbff920764
jilara <78088103+jilara@users.noreply.github.com>
2024-03-27 15:45:22 -0400

    project3

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 <a href="helloworld/index.html" target="_blank">Github Project 1</a> 
 
 <a href="jquery1/index.html" target="_blank">project 2</a> 
+<a href="jquery2/index.html" target="_blank">project 3</a> 
---
+++ jquery2/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    // Image swapping in gallery
+    $('.gallery-item').click(function(){
+        var $img = $(this).find('img');
+        var originalSrc = $img.attr('src');
+        var altSrc = $(this).data('alt-src');
+        
+        // Check if the current source is the original or alternative
+        if ($img.data('current-src') === 'original') {
+            $img.attr('src', altSrc);
+            $img.data('current-src', 'alternative');
+        } else {
+            $img.attr('src', originalSrc);
+            $img.data('current-src', 'original');
+        }
+    });
+});
